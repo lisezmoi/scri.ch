4620db871df4390cc86923a0f3b6bc4924b28a12
Pierre Bertet <bonjour@pierrebertet.net>
2013-01-04 20:28:51 +0100

    Fullscreen on iOS

--- lib/template.php
+++ lib/template.php
@@ -36,6 +36,7 @@ CONTACT
 		<link rel="icon" type="image/png" href="<?php echo SCRICH_URL ?>assets/favicon.png">
 		<link rel="apple-touch-icon" href="<?php echo SCRICH_URL ?>assets/apple-touch-icon.png">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
+		<meta name="apple-mobile-web-app-capable" content="yes">
 		<style><?php if ($cur_img): ?>#save{display:none;}<?php else: ?>#buttons button,#about{display:none;}<?php endif; ?></style>
 	</head>
 	<body>
